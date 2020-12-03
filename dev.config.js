fdd01baade63d1ff2132a814326f7b4854a8d0b7
alexx-gsm <alexx-gsm@yandex.ru>
2020-12-03 17:22:36 +0700

    rm dest

--- dev.config.js
+++ dev.config.js
@@ -1,21 +1,34 @@
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dest'),
+    filename: 'bundle.[hash].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, 'src'),
+        exclude: path.resolve(__dirname, 'src/assets'),
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{from: 'src/assets', to: ''}],
+    }),
+  ],
 }
