f6c7aedb2106e084505a910e0cee3063df284354
alexx-gsm <alexx-gsm@yandex.ru>
2021-01-06 21:38:28 +0700

    update render Table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,59 +6,6 @@ class Table extends ExcelComponent {
 
   toHTML() {
     return createTable()
-    // return `
-    //   <div class="table-row">
-    //     <div class="info"></div>
-    //     <div class="data">
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //       <div class="column column-header">A</div>
-    //       <div class="column column-header">B</div>
-    //       <div class="column column-header">C</div>
-    //     </div>
-    //   </div>
-    //   <div class="table-row">
-    //     <div class="info">1</div>
-    //     <div class="data">
-    //       <div class="column" contenteditable></div>
-    //       <div class="column" contenteditable></div>
-    //       <div class="column" contenteditable></div>
-    //     </div>
-    //   </div>
-    //   <div class="table-row">
-    //     <div class="info">2</div>
-    //     <div class="data">
-    //       <div class="column" contenteditable></div>
-    //       <div class="column selected" contenteditable></div>
-    //       <div class="column" contenteditable></div>
-    //     </div>
-    //   </div>
-    //   <div class="table-row">
-    //     <div class="info">3</div>
-    //     <div class="data">
-    //       <div class="column" contenteditable></div>
-    //       <div class="column" contenteditable></div>
-    //       <div class="column" contenteditable></div>
-    //     </div>
-    //   </div>
-    // </div>
-    // `
   }
 }
 
